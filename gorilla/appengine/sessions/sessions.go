02da10ff278794376b78d953f5189c572d7e577a
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2011-09-20 18:33:54 -0300

    Refactoring on App Engine session stores; reusing common code.

--- gorilla/appengine/sessions/sessions.go
+++ gorilla/appengine/sessions/sessions.go
@@ -15,6 +15,21 @@ import (
 	"gorilla.googlecode.com/hg/gorilla/sessions"
 )
 
+type baseSessionStore struct {
+	// List of encoders registered for this store.
+	encoders []sessions.SessionEncoder
+}
+
+// Encoders returns the encoders for this store.
+func (s *baseSessionStore) Encoders() []sessions.SessionEncoder {
+	return s.encoders
+}
+
+// SetEncoders sets a group of encoders in the store.
+func (s *baseSessionStore) SetEncoders(encoders ...sessions.SessionEncoder) {
+	s.encoders = encoders
+}
+
 // ----------------------------------------------------------------------------
 // DatastoreSessionStore
 // ----------------------------------------------------------------------------
@@ -26,8 +41,7 @@ type Session struct {
 
 // DatastoreSessionStore stores session data in App Engine's datastore.
 type DatastoreSessionStore struct {
-	// List of encoders registered for this store.
-	encoders []sessions.SessionEncoder
+	baseSessionStore
 }
 
 // Load loads a session for the given key.
@@ -40,7 +54,7 @@ func (s *DatastoreSessionStore) Load(r *http.Request, key string,
 		for k, _ := range data {
 			data[k] = nil, false
 		}
-		// Get session from memcache and deserialize it.
+		// Get session from datastore and deserialize it.
 		c := appengine.NewContext(r)
 		var session Session
 		key := datastore.NewKey("Session", sessionKey(sid), 0, nil)
@@ -55,45 +69,23 @@ func (s *DatastoreSessionStore) Load(r *http.Request, key string,
 // Save saves the session in the response.
 func (s *DatastoreSessionStore) Save(r *http.Request, w http.ResponseWriter,
 									 key string, info *sessions.SessionInfo) (flag bool, err os.Error) {
-	// Create a new session id.
-	var sid string
-	sid, err = sessions.GenerateSessionId(128)
-	if err != nil {
+	sid, serialized, error := getIdAndData(info)
+	if error != nil {
+		err = error
 		return
 	}
-	// Serialize session into []byte.
-	var serialized []byte
-	serialized, err = sessions.SerializeSessionData(&info.Data)
-	if err != nil {
-		return
-	}
+
 	// Save the session.
-	c := appengine.NewContext(r)
 	entityKey := datastore.NewKey("Session", sessionKey(sid), 0, nil)
-	session := Session{
+	_, err = datastore.Put(appengine.NewContext(r), entityKey, &Session{
 		Date:  datastore.SecondsToTime(time.Seconds()),
 		Value: serialized,
-	}
-	if entityKey, err = datastore.Put(c, entityKey, &session); err != nil {
+	})
+	if err != nil {
 		return
 	}
-	// Clone info, setting only sid in data.
-	newinfo := &sessions.SessionInfo{
-		Data:   sessions.SessionData{"sid": sid},
-		Store:  info.Store,
-		Config: info.Config,
-	}
-	return sessions.SetCookie(s, w, key, newinfo)
-}
 
-// Encoders returns the encoders for this store.
-func (s *DatastoreSessionStore) Encoders() []sessions.SessionEncoder {
-	return s.encoders
-}
-
-// SetEncoders sets a group of encoders in the store.
-func (s *DatastoreSessionStore) SetEncoders(encoders ...sessions.SessionEncoder) {
-	s.encoders = encoders
+	return sessions.SetCookie(s, w, key, cloneInfo(info, sid))
 }
 
 // ----------------------------------------------------------------------------
@@ -102,8 +94,7 @@ func (s *DatastoreSessionStore) SetEncoders(encoders ...sessions.SessionEncoder)
 
 // MemcacheSessionStore stores session data in App Engine's memcache.
 type MemcacheSessionStore struct {
-	// List of encoders registered for this store.
-	encoders []sessions.SessionEncoder
+	baseSessionStore
 }
 
 // Load loads a session for the given key.
@@ -129,46 +120,48 @@ func (s *MemcacheSessionStore) Load(r *http.Request, key string,
 // Save saves the session in the response.
 func (s *MemcacheSessionStore) Save(r *http.Request, w http.ResponseWriter,
 									key string, info *sessions.SessionInfo) (flag bool, err os.Error) {
+	sid, serialized, error := getIdAndData(info)
+	if error != nil {
+		err = error
+		return
+	}
+
+	// Add the item to the memcache, if the key does not already exist.
+	err = memcache.Add(appengine.NewContext(r), &memcache.Item{
+		Key:   sessionKey(sid),
+		Value: serialized,
+	})
+	if err != nil {
+		return
+	}
+
+	return sessions.SetCookie(s, w, key, cloneInfo(info, sid))
+}
+
+func sessionKey(sid string) string {
+	return fmt.Sprintf("gorilla.appengine.sessions.%s", sid)
+}
+
+// Create a new sid and serialize data.
+func getIdAndData(info *sessions.SessionInfo) (sid string, serialized []byte, err os.Error) {
 	// Create a new session id.
-	var sid string
 	sid, err = sessions.GenerateSessionId(128)
 	if err != nil {
 		return
 	}
 	// Serialize session into []byte.
-	var serialized []byte
 	serialized, err = sessions.SerializeSessionData(&info.Data)
 	if err != nil {
 		return
 	}
-	// Add the item to the memcache, if the key does not already exist.
-	c := appengine.NewContext(r)
-	item := &memcache.Item{
-		Key:   sessionKey(sid),
-		Value: serialized,
-	}
-	if err := memcache.Add(c, item); err != nil {
-		return false, err
-	}
-	// Clone info, setting only sid in data.
-	newinfo := &sessions.SessionInfo{
+	return
+}
+
+// Clone info, setting only sid as data.
+func cloneInfo(info *sessions.SessionInfo, sid string) *sessions.SessionInfo {
+	return &sessions.SessionInfo{
 		Data:   sessions.SessionData{"sid": sid},
 		Store:  info.Store,
 		Config: info.Config,
 	}
-	return sessions.SetCookie(s, w, key, newinfo)
-}
-
-// Encoders returns the encoders for this store.
-func (s *MemcacheSessionStore) Encoders() []sessions.SessionEncoder {
-	return s.encoders
-}
-
-// SetEncoders sets a group of encoders in the store.
-func (s *MemcacheSessionStore) SetEncoders(encoders ...sessions.SessionEncoder) {
-	s.encoders = encoders
-}
-
-func sessionKey(sid string) string {
-	return fmt.Sprintf("gorilla.appengine.sessions.%s", sid)
 }
