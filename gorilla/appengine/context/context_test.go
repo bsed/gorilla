5867c2adf5188d993b94d4a16a95f93d73a01088
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2011-12-08 19:40:01 -0200

    Added a convenience package to create custom App Engine contexts.

---
+++ gorilla/appengine/context/context_test.go
@@ -0,0 +1,13 @@
+// Copyright 2011 Gorilla Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package context
+
+import (
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	// Nothing here, yet. Need to figure how to setup proper stubs.
+}
