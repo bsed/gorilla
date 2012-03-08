f1762f9b72c29706c8ca793ad3a60d34594f0a8f
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2012-03-08 07:45:29 -0300

    sessions: updated to new version.

--- sessions/doc.go
+++ sessions/doc.go
@@ -4,168 +4,123 @@
 
 /*
 Package gorilla/sessions provides cookie sessions and infrastructure for
-custom session back-ends.
+custom session backends.
 
 The key features are:
 
-	* Dead simple basic API: use it as an easy way to set signed (and
-	  optionally encrypted) cookies.
-	* Advanced API for custom back-ends: built-in support for custom storage
-	  systems; session store interface and helper functions; encoder interface
-	  and default implementation with customizable cryptography methods
-	  (thanks to Go interfaces).
-	* Conveniences: flash messages (session values that last until read);
-	  built-in mechanism to rotate authentication and encryption keys;
-	  multiple sessions per request, even using different back-ends;
-	  easy way to switch session persistency (aka "remember me") and set
+	* Simple API: use it as an easy way to set signed (and optionally
+	  encrypted) cookies.
+	* Built-in backends to store sessions in cookies or the filesystem.
+	* Flash messages: session values that last until read.
+	* Convenient way to switch session persistency (aka "remember me") and set
 	  other attributes.
+	* Mechanism to rotate authentication and encryption keys.
+	* Multiple sessions per request, even using different backends.
+	* Interfaces and infrastructure for custom session backends: sessions from
+	  different stores can be retrieved and batch-saved using a common API.
 
-The most basic example to retrieve a session is to call sessions.Session()
-passing the current request. For example, in a handler:
+Let's start with an example that shows the sessions API in a nutshell:
+
+	import (
+		"net/http"
+		"code.google.com/p/gorilla/sessions"
+	)
+
+	var store = sessions.NewCookieStore([]byte("something-very-secret"))
 
 	func MyHandler(w http.ResponseWriter, r *http.Request) {
-		if session, err := sessions.Session(r); err == nil {
-			session["foo"] = "bar"
-			session["baz"] = 128
-			sessions.Save(r, w)
+		// Get a session. We're ignoring the error resulted from decoding an
+		// existing session: Get() always returns a session, even if empty.
+		session, _ := store.Get(r, "session-name")
+		// Set some session values.
+		session.Values["foo"] = "bar"
+		session.Values[42] = 43
+		// Save it.
+		session.Save(r, w)
+	}
+
+First we initialize a session store calling NewCookieStore() and passing a
+secret key used to authenticate the session. Inside the handler, we call
+store.Get() to retrieve an existing session or a new one. Then we set some
+session values in session.Values, which is a map[interface{}]interface{}.
+And finally we call session.Save() to save the session in the response.
+
+That's all you need to know for the basic usage. Let's take a look at other
+options, starting with flash messages.
+
+Flash messages are session values that last until read. The term appeared with
+Ruby On Rails a few years back. When we request a flash message, it is removed
+from the session. To add a flash, call session.AddFlash(), and to get all
+flashes, call session.Flashes(). Here is an example:
+
+	func MyHandler(w http.ResponseWriter, r *http.Request) {
+		// Get a session.
+		session, _ := store.Get(r, "session-name")
+		// Get the previously flashes, if any.
+		if flashes := session.Flashes(); len(flashes) > 0 {
+			// Just print the flash values.
+			fmt.Fprint(w, "%v", flashes)
+		} else {
+			// Set a new flash.
+			session.AddFlash("Hello, flash messages world!")
+			fmt.Fprint(w, "No flashes found.")
 		}
+		session.Save(r, w)
 	}
 
-The above snippet is "gorilla/sessions in a nutshell": a session is a simple
-map[string]interface{} returned from sessions.Session(). It stores any values
-that can be encoded using gob. After we set some values, we call
-sessions.Save() passing the current request and response.
+Flash messages are useful to set information to be read after a redirection,
+like after form submissions.
 
-Side note about "any values that can be encoded using gob": to store special
-structures in a session, we must register them using gob.Register() first.
-For basic types this is not needed; it works out of the box.
+By default, session cookies last for a month. This is probably too long for
+some cases, but it is easy to change this and other attributes during
+runtime. Sessions can be configured individually or the store can be
+configured and then all sessions saved using it will use that configuration.
+We access session.Options or store.Options to set a new configuration. The
+fields are basically a subset of http.Cookie fields. Let's change the
+maximum age of a session to one week:
 
-Is it that simple? Well, almost. Before we can use sessions, we must define
-a secret key to be used for authentication, and optionally an encryption key.
-They are both set calling SetStoreKeys() and should be done at initialization
-time:
-
-	func init() {
-		sessions.SetStoreKeys("cookie",
-							  []byte("my-hmac-key"),
-							  []byte("my-aes-key"))
+	session.Options = &sessions.Options{
+		Path:   "/",
+		MaxAge: 86400 * 7,
 	}
-
-The first argument is the name used to register the session store. By default
-a "cookie" store is registered and available for use, so we use that name.
-
-The second argument is the secret key used to authenticate the session cookie
-using HMAC. It is required; if no authentication key is set sessions can't be
-read or written (and a call to sessions.Session() will return an error).
-
-The third argument is the encryption key; it is optional and can be omitted.
-For the cookie store, setting this will encrypt the contents stored in the
-cookie; otherwise the contents can be read, although not forged.
-
-Side note about the encryption key: if set, must be either 16, 24, or 32 bytes
-to select AES-128, AES-192, or AES-256 modes. Otherwise a block can't be
-created and sessions won't work.
-
-Exposing the contents of a session is not a big deal in many cases, like when
-we store a simple username or user id, but to to store sensitive information
-using the cookie store, we must set an encryption key. For custom stores that
-only set a random session id in the cookie, encryption is not needed.
-
-And this is all you need to know about the basic usage. More advanced options
-are explained below.
 
 Sometimes we may want to change authentication and/or encryption keys without
-breaking existing sessions. We can do this setting multiple authentication and
-encryption keys, in pairs, to be tested in order:
+breaking existing sessions. The CookieStore supports key rotation, and to use
+it you just need to set multiple authentication and encryption keys, in pairs,
+to be tested in order:
 
-	sessions.SetStoreKeys("cookie",
-						  []byte("my-hmac-key"),
-						  []byte("my-aes-key"),
-						  []byte("my-previous-hmac-key"),
-						  []byte("my-previous-aes-key"))
+	var store = sessions.NewCookieStore(
+		[]byte("new-authentication-key"),
+		[]byte("new-encryption-key"),
+		[]byte("old-authentication-key"),
+		[]byte("old-encryption-key"),
+	)
 
 New sessions will be saved using the first pair. Old sessions can still be
 read because the first pair will fail, and the second will be tested. This
 makes it easy to "rotate" secret keys and still be able to validate existing
-sessions. Note: for all pairs the encryption key is optional; set it
-to nil and encryption won't be used.
+sessions. Note: for all pairs the encryption key is optional; set it to nil
+or omit it and and encryption won't be used.
 
-Back to how sessions are retrieved.
+Multiple sessions can be used in the same request, even with different
+session backends. When this happens, calling Save() on each session
+individually would be cumbersome, so we have a way to save all sessions
+at once: it's sessions.Save(). Here's an example:
 
-Sessions are named. When we get a session calling sessions.Session(request),
-we are implicitly requesting a session using the default key ("s") and store
-(the CookieSessionStore). This is just a convenience; we can have as many
-sessions as needed, just passing different session keys. For example:
-
-	if authSession, err := sessions.Session(r, "auth"); err == nil {
-		userId = authSession["userId"]
-		// ...
-	}
-
-Here we requested a session explicitly naming it "auth". It will be saved
-separately. This can be used as a convenient way to save signed cookies, and
-is also how we access multiple sessions per request.
-
-Session stores also have a name, and need to be registered to be available.
-The default session store uses authenticated (and optionally encrypted)
-cookies, and is registered with the name "cookie". To use a custom
-session back-end, we first register it in the SessionFactory, then pass its
-name as the third argument to sessions.Session().
-
-For the sake of demonstration, let's pretend that we defined a store called
-MemcacheSessionStore. First we register it using the "memcache" key. This
-should be done at initialization time:
-
-	func init() {
-		sessions.SetStore("memcache", new(MemcacheSessionStore))
-	}
-
-Then to get a session using memcached, we pass a third argument to
-sessions.Session(), the store key:
-
-	session, err := sessions.Session(r, "mysession", "memcache")
-
-...and it will use the custom back-end we defined, instead of the default
-"cookie" one. This means that we can use multiple sessions in the same
-request even using different back-ends.
-
-And how to configure session expiration time, path or other cookie attributes?
-
-By default, session cookies last for a month. This is probably too long for a
-lot of cases, but it is easy to change this and other attributes during
-runtime. Just request the session configuration struct and change the variables
-as needed. The fields are basically a subset of http.Cookie fields.
-To change MaxAge, we would do:
-
-	if config, err = sessions.Config(r); err == nil {
-		// Change max-age to 1 week.
-		config.MaxAge = 86400 * 7
-	}
-
-After this, cookies will last for a week only. The Config() function
-accepts an optional argument besides the request: the session key. If not
-defined, the configuration for the default session key is returned.
-
-Bonus: flash messages. What are they? It basically means "session values that
-last until read". The term was popularized by Ruby On Rails a few years back.
-When we request a flash message, it is removed from the session. We have two
-convenience functions to read and set them: Flashes() and AddFlash(). Here is
-an example:
+	var store = sessions.NewCookieStore([]byte("something-very-secret"))
 
 	func MyHandler(w http.ResponseWriter, r *http.Request) {
-		// Get the previously set flashes, if any.
-		if flashes, _ := sessions.Flashes(r); flashes != nil {
-			// Just print the flash values.
-			fmt.Fprint(w, "%v", flashes)
-		} else {
-			fmt.Fprint(w, "No flashes found.")
-			// Set a new flash.
-			sessions.AddFlash(r, "Hello, flash messages world!")
-		}
+		// Get a session and set a value.
+		session1, _ := store.Get(r, "session-one")
+		session1.Values["foo"] = "bar"
+		// Get another session and set another value.
+		session2, _ := store.Get(r, "session-two")
+		session2.Values[42] = 43
+		// Save all sessions.
 		sessions.Save(r, w)
 	}
 
-Flash messages are useful to set information to be read after a redirection,
-usually after form submissions.
+This is possible because when we call Get() from a session store, it adds the
+session to a common registry. Save() uses it to save all registered sessions.
 */
 package sessions
