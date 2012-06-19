805b8bb9deddebb9d7f16580eab2dfda2ba34399
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2012-06-19 12:22:39 -0300

    simpler name.

--- reverse/reverse.go
+++ reverse/reverse.go
@@ -7,11 +7,11 @@ import (
 	"regexp/syntax"
 )
 
-// ReverseRegexp stores a regular expression that can be "reverted" or "built":
+// Regexp stores a regular expression that can be "reverted" or "built":
 // outermost capturing groups become placeholders to be filled by variables.
 //
-// For example, given a ReverseRegexp with the pattern `1(\d+)3`, we can
-// call re.Revert([]string{"2"}, nil) to get a resulting string "123".
+// For example, given a Regexp with the pattern `1(\d+)3`, we can call
+// re.Revert([]string{"2"}, nil) to get a resulting string "123".
 // This also works for named capturing groups: we can revert `1(?P<two>\d+)3`
 // calling re.Revert(nil, map[string]string{"two": "2"}).
 //
@@ -24,7 +24,7 @@ import (
 //
 // 2. Literals inside capturing groups are ignored; the whole group becomes
 // a placeholder.
-type ReverseRegexp struct {
+type Regexp struct {
 	compiled *regexp.Regexp // compiled regular expression
 	template string         // reverse template
 	groups   []string       // order of positional and named capturing groups;
@@ -33,7 +33,7 @@ type ReverseRegexp struct {
 
 // Compile compiles the regular expression pattern and creates a template
 // to revert it.
-func Compile(pattern string) (*ReverseRegexp, error) {
+func Compile(pattern string) (*Regexp, error) {
 	re, err := syntax.Parse(pattern, syntax.Perl)
 	if err != nil {
 		return nil, err
@@ -41,7 +41,7 @@ func Compile(pattern string) (*ReverseRegexp, error) {
 	var template bytes.Buffer
 	var groups []string
 	writeTemplate(&template, &groups, re)
-	return &ReverseRegexp{
+	return &Regexp{
 		compiled: regexp.MustCompile(pattern),
 		template: template.String(),
 		groups:   groups,
@@ -49,7 +49,7 @@ func Compile(pattern string) (*ReverseRegexp, error) {
 }
 
 // Regexp returns the compiled regular expression to be used for matching.
-func (r *ReverseRegexp) Regexp() *regexp.Regexp {
+func (r *Regexp) Regexp() *regexp.Regexp {
 	return r.compiled
 }
 
@@ -58,7 +58,7 @@ func (r *ReverseRegexp) Regexp() *regexp.Regexp {
 //
 // Positional groups are listed as an empty string and named groups use
 // the group name.
-func (r *ReverseRegexp) Groups() []string {
+func (r *Regexp) Groups() []string {
 	return r.groups
 }
 
@@ -67,7 +67,7 @@ func (r *ReverseRegexp) Groups() []string {
 // The args parameter is used for positional and named capturing groups,
 // and the kwds parameter is optionally used for named groups only;
 // if a name is not provided in kwds, the value is taken from args, in order.
-func (r *ReverseRegexp) Revert(args []string, kwds map[string]string) (string, error) {
+func (r *Regexp) Revert(args []string, kwds map[string]string) (string, error) {
 	i := 0
 	values := make([]interface{}, len(r.groups))
 	for k, v := range r.groups {
@@ -91,7 +91,7 @@ func (r *ReverseRegexp) Revert(args []string, kwds map[string]string) (string, e
 
 // ValidRevert is the same as Revert but it also validates the resulting
 // string matching it against the compiled regexp.
-func (r *ReverseRegexp) ValidRevert(args []string, kwds map[string]string) (string, error) {
+func (r *Regexp) ValidRevert(args []string, kwds map[string]string) (string, error) {
 	reverse, err := r.Revert(args, kwds)
 	if err != nil {
 		return "", err
