7c865c1cc3eb548a920aade522f3b5a418de4067
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2012-06-20 20:49:36 -0300

    gettext: some tests and more docs.

---
+++ gettext/gettext_test.go
@@ -0,0 +1,65 @@
+// Copyright 2012 The Gorilla Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package gettext
+
+import (
+	"bytes"
+	"testing"
+	"encoding/base64"
+)
+
+// From Python's gettext tests
+var gnuMoData = `3hIElQAAAAAGAAAAHAAAAEwAAAALAAAAfAAAAAAAAACoAAAAFQAAAKkAAAAjAAAAvwAAAKEAAADj
+AAAABwAAAIUBAAALAAAAjQEAAEUBAACZAQAAFgAAAN8CAAAeAAAA9gIAAKEAAAAVAwAABQAAALcD
+AAAJAAAAvQMAAAEAAAADAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAABQAAAAYAAAACAAAAAFJh
+eW1vbmQgTHV4dXJ5IFlhY2gtdABUaGVyZSBpcyAlcyBmaWxlAFRoZXJlIGFyZSAlcyBmaWxlcwBU
+aGlzIG1vZHVsZSBwcm92aWRlcyBpbnRlcm5hdGlvbmFsaXphdGlvbiBhbmQgbG9jYWxpemF0aW9u
+CnN1cHBvcnQgZm9yIHlvdXIgUHl0aG9uIHByb2dyYW1zIGJ5IHByb3ZpZGluZyBhbiBpbnRlcmZh
+Y2UgdG8gdGhlIEdOVQpnZXR0ZXh0IG1lc3NhZ2UgY2F0YWxvZyBsaWJyYXJ5LgBtdWxsdXNrAG51
+ZGdlIG51ZGdlAFByb2plY3QtSWQtVmVyc2lvbjogMi4wClBPLVJldmlzaW9uLURhdGU6IDIwMDAt
+MDgtMjkgMTI6MTktMDQ6MDAKTGFzdC1UcmFuc2xhdG9yOiBKLiBEYXZpZCBJYsOhw7FleiA8ai1k
+YXZpZEBub29zLmZyPgpMYW5ndWFnZS1UZWFtOiBYWCA8cHl0aG9uLWRldkBweXRob24ub3JnPgpN
+SU1FLVZlcnNpb246IDEuMApDb250ZW50LVR5cGU6IHRleHQvcGxhaW47IGNoYXJzZXQ9aXNvLTg4
+NTktMQpDb250ZW50LVRyYW5zZmVyLUVuY29kaW5nOiBub25lCkdlbmVyYXRlZC1CeTogcHlnZXR0
+ZXh0LnB5IDEuMQpQbHVyYWwtRm9ybXM6IG5wbHVyYWxzPTI7IHBsdXJhbD1uIT0xOwoAVGhyb2F0
+d29iYmxlciBNYW5ncm92ZQBIYXkgJXMgZmljaGVybwBIYXkgJXMgZmljaGVyb3MAR3V2ZiB6YnFo
+eXIgY2ViaXZxcmYgdmFncmVhbmd2YmFueXZtbmd2YmEgbmFxIHlicG55dm1uZ3ZiYQpmaGNjYmVn
+IHNiZSBsYmhlIENsZ3ViYSBjZWJ0ZW56ZiBvbCBjZWJpdnF2YXQgbmEgdmFncmVzbnByIGdiIGd1
+ciBUQUgKdHJnZ3JrZyB6cmZmbnRyIHBuZ255YnQgeXZvZW5lbC4AYmFjb24Ad2luayB3aW5rAA==`
+
+func decode(value []byte) ([]byte, error) {
+	decoded := make([]byte, base64.StdEncoding.DecodedLen(len(value)))
+	b, err := base64.StdEncoding.Decode(decoded, value)
+	if err != nil {
+		return nil, err
+	}
+	return decoded[:b], nil
+}
+
+func TestGettext(t *testing.T) {
+	b, err := decode([]byte(gnuMoData))
+	if err != nil {
+		t.Fatal(err)
+	}
+	c, err := NewCatalog(bytes.NewReader(b))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	equalString := func(trans, expected string) {
+		if trans != expected {
+			t.Errorf("Expected %q, got %q.", expected, trans)
+		}
+	}
+	// gettext
+	equalString(c.Gettext("albatross"), "albatross")
+	equalString(c.Gettext("mullusk"), "bacon")
+	equalString(c.Gettext("Raymond Luxury Yach-t"), "Throatwobbler Mangrove")
+	equalString(c.Gettext("nudge nudge"), "wink wink")
+	equalString(c.Gettext("There is %s file"), "Hay %s fichero")
+	// ngettext
+	equalString(c.Ngettext("There is %s file", "There is %s file", 1), "Hay %s fichero")
+	equalString(c.Ngettext("There is %s file", "There are %s files", 2), "Hay %s ficheros")
+}
