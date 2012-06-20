6507b374b198f3204c3c312c161ba406f1a46f7e
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2012-06-20 20:49:36 -0300

    gettext: some tests and more docs.

--- gettext/gettext.go
+++ gettext/gettext.go
@@ -25,6 +25,8 @@ type Reader interface {
 // Catalog stores translations for a GNU MO file.
 //
 // Inspired by Python's gettext.GNUTranslations.
+//
+// TODO: Gettextf(msg, replacements...) to use with fmt.Sprintf?
 type Catalog struct {
 	Fallback *Catalog
 	Contexts map[string]string
@@ -32,6 +34,7 @@ type Catalog struct {
 	Plurals  map[string][]string
 }
 
+// Gettext returns a translation for msg.
 func (c *Catalog) Gettext(msg string) string {
 	if trans, ok := c.Messages[msg]; ok {
 		return trans
@@ -42,10 +45,10 @@ func (c *Catalog) Gettext(msg string) string {
 	return msg
 }
 
+// Ngettext returns a plural translation for msg1 according to the amount n.
 func (c *Catalog) Ngettext(msg1, msg2 string, n int) string {
 	if plurals, ok := c.Plurals[msg1]; ok {
-		idx := c.pluralIndex(n)
-		if idx < len(plurals) {
+		if idx := c.pluralIndex(n); idx < len(plurals) {
 			return plurals[idx]
 		}
 	}
@@ -58,9 +61,19 @@ func (c *Catalog) Ngettext(msg1, msg2 string, n int) string {
 	return msg2
 }
 
+// pluralIndex returns the index of the plural form for the amount n.
+//
+// This depends on parsing the Plural-Forms header and is still not supported.
+// E.g., expression for English and many others:
+//
+//     Plural-Forms: nplurals=2; plural=n != 1;
+//
+// ...which translates to the exoression in the body of this function.
 func (c *Catalog) pluralIndex(n int) int {
-	// TODO
-	return n
+	if n != 1 {
+		return 1
+	}
+	return 0
 }
 
 // New catalog builds a translations catalog from a GNU MO file contents.
@@ -68,8 +81,6 @@ func (c *Catalog) pluralIndex(n int) int {
 // GNU MO file format specification:
 //
 //     http://www.gnu.org/software/gettext/manual/gettext.html#MO-Files
-//
-// TODO: check if the format version is supported
 func NewCatalog(r Reader) (*Catalog, error) {
 	c := &Catalog{
 		Contexts: make(map[string]string),
@@ -82,22 +93,22 @@ func NewCatalog(r Reader) (*Catalog, error) {
 	return c, nil
 }
 
-// WriteCatalog parses the GNU MO contents from the reader and writes it to
-// the given catalog.
+// WriteCatalog parses the GNU MO contents from the reader and writes
+// the messages and translations to the given catalog.
 //
 // GNU MO file format specification:
 //
 //     http://www.gnu.org/software/gettext/manual/gettext.html#MO-Files
 //
 // TODO: check if the format version is supported
-// TODO: parse file metadata
+// TODO: parse file header; specially Content-Type and Plural-Forms values.
 func WriteCatalog(c *Catalog, r Reader) error {
 	// First word identifies the byte order.
+	var order binary.ByteOrder
 	var magic uint32
 	if err := binary.Read(r, binary.LittleEndian, &magic); err != nil {
 		return err
 	}
-	var order binary.ByteOrder
 	if magic == magicLittleEndian {
 		order = binary.LittleEndian
 	} else if magic == magicBigEndian {
@@ -124,7 +135,7 @@ func WriteCatalog(c *Catalog, r Reader) error {
 	// same lookup table, per spec.
 	var mLen, mIdx, tLen, tIdx uint32
 	for i := 0; i < int(count); i++ {
-		// The original message length and position.
+		// Get original message length and position.
 		r.Seek(int64(mTableIdx), 0)
 		if err := binary.Read(r, order, &mLen); err != nil {
 			return err
@@ -132,7 +143,12 @@ func WriteCatalog(c *Catalog, r Reader) error {
 		if err := binary.Read(r, order, &mIdx); err != nil {
 			return err
 		}
-		// The translation length and position.
+		// Get original message.
+		m := make([]byte, mLen)
+		if _, err := r.ReadAt(m, int64(mIdx)); err != nil {
+			return err
+		}
+		// Get translation length and position.
 		r.Seek(int64(tTableIdx), 0)
 		if err := binary.Read(r, order, &tLen); err != nil {
 			return err
@@ -140,17 +156,19 @@ func WriteCatalog(c *Catalog, r Reader) error {
 		if err := binary.Read(r, order, &tIdx); err != nil {
 			return err
 		}
-		// The original message.
-		s := make([]byte, mLen)
-		if _, err := r.ReadAt(s, int64(mIdx)); err != nil {
-			return err
-		}
-		// The translation.
+		// Get translation.
 		t := make([]byte, tLen)
 		if _, err := r.ReadAt(t, int64(tIdx)); err != nil {
 			return err
 		}
-		mStr, tStr := string(s), string(t)
+		// Move cursor to next string.
+		mTableIdx += 8
+		tTableIdx += 8
+		mStr, tStr := string(m), string(t)
+		if mStr == "" {
+			// TODO: this is the file header. Parse it.
+			continue
+		}
 		// Check for context.
 		ctx := ""
 		if cIdx := strings.Index(mStr, "\x04"); cIdx != -1 {
@@ -160,7 +178,7 @@ func WriteCatalog(c *Catalog, r Reader) error {
 		// Check for plurals.
 		if pIdx := strings.Index(mStr, "\x00"); pIdx != -1 {
 			// Store only the singular of the original string and translation
-			// in the strings map, and all plural forms in the plurals map.
+			// in the messages map, and all plural forms in the plurals map.
 			mStr = mStr[:pIdx]
 			tPlurals := strings.Split(tStr, "\x00")
 			c.Messages[mStr] = tPlurals[0]
@@ -171,9 +189,6 @@ func WriteCatalog(c *Catalog, r Reader) error {
 		if ctx != "" {
 			c.Contexts[mStr] = ctx
 		}
-		// Move to next string.
-		mTableIdx += 8
-		tTableIdx += 8
 	}
 	return nil
 }
