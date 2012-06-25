3d93332e78cccc40250387eaa042e91190f7755f
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2012-06-24 23:57:23 -0300

    pluralforms: tests were using precomputed functions. duh. now it is fixed.

--- gettext/pluralforms/pluralforms_test.go
+++ gettext/pluralforms/pluralforms_test.go
@@ -13,17 +13,18 @@ func TestParse(t *testing.T) {
 	// functions.
 	fNumber := 1
 	for expr, fn := range pluralFuncs {
-		fn2, err := Parse(expr)
+		fn2, err := createPluralFunc(expr)
 		if err != nil {
-			t.Fatal(err)
-		}
-		for i := 0; i < 200; i++ {
-			expected := fn(i)
-			result := fn2(i)
-			if result != expected {
-				t.Fatalf("pluralFunc%d: expected %d, got %d during iteration %d.", fNumber, expected, result, i, expr)
+			t.Errorf("pluralFunc%d: failed to parse %q (%s).", fNumber, expr, err)
+		} else {
+			for i := 0; i < 200; i++ {
+				expected := fn(i)
+				result := fn2(i)
+				if result != expected {
+					t.Errorf("pluralFunc%d: expected %d, got %d during iteration %d.", fNumber, expected, result, i, expr)
+				}
+				t.Logf("pluralFunc%d: expected %d, got %d during iteration %d.", fNumber, expected, result, i)
 			}
-			t.Logf("pluralFunc%d: expected %d, got %d during iteration %d.", fNumber, expected, result, i)
 		}
 		fNumber++
 	}
@@ -40,7 +41,7 @@ func TestParse(t *testing.T) {
 		"2 * (3 * (4 + 5)))",
 	}
 	for _, expr := range badExprs {
-		fn, err := Parse(expr)
+		fn, err := createPluralFunc(expr)
 		if err == nil {
 			for i := 0; i < 200; i++ {
 				expected := -1
