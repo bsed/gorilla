9e24da29bed2ab03138e663574639c43f871b2c7
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2012-06-24 23:23:37 -0300

    pluralforms: added some tests for bad expressions.

--- gettext/pluralforms/pluralforms_test.go
+++ gettext/pluralforms/pluralforms_test.go
@@ -9,6 +9,8 @@ import (
 )
 
 func TestParse(t *testing.T) {
+	// Compare the results of the parsed expression and the precomputed
+	// functions.
 	fNumber := 1
 	for expr, fn := range pluralFuncs {
 		fn2, err := Parse(expr)
@@ -19,10 +21,34 @@ func TestParse(t *testing.T) {
 			expected := fn(i)
 			result := fn2(i)
 			if result != expected {
-				t.Fatalf("Expected %d, got %d during iteration %d. Expression: %s", expected, result, i, expr)
+				t.Fatalf("pluralFunc%d: expected %d, got %d during iteration %d.", fNumber, expected, result, i, expr)
 			}
 			t.Logf("pluralFunc%d: expected %d, got %d during iteration %d.", fNumber, expected, result, i)
 		}
 		fNumber++
+	}
+	// Now some bad expressions.
+	badExprs := []string{
+		"1 *",
+		"-1 * 2", // negative numbers are not allowed
+		"1 (1)",
+		"1 ?",
+		"1 ? 2",
+		"1 :",
+		"1 : 2",
+		"2 * (3 * (4 + 5)",
+		"2 * (3 * (4 + 5)))",
+	}
+	for _, expr := range badExprs {
+		fn, err := Parse(expr)
+		if err == nil {
+			for i := 0; i < 200; i++ {
+				expected := -1
+				result := fn(i)
+				if result != expected {
+					t.Fatalf("Expected %d, got %d during iteration %d. Expression: %s", expected, result, i, expr)
+				}
+			}
+		}
 	}
 }
