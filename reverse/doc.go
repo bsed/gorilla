601a8f3143266c58b2a006a129b8644d1bffdde3
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2012-08-20 16:06:53 -0300

    reverse: updated docs.

--- reverse/doc.go
+++ reverse/doc.go
@@ -10,26 +10,28 @@ For example, let's start compiling a simple regexp:
 
 	regexp, err := reverse.Compile(`/foo/1(\d+)3`)
 
-Now we can call regexp.Revert() passing variables to fill the capturing groups:
+Now we can call regexp.Revert() passing variables to fill the capturing groups.
+Because our variable is not named, we use an empty string as key for
+url.Values, like this:
 
 	// url is "/foo/123".
-	url, err := regexp.Revert([]string{"2"}, nil)
+	url, err := regexp.Revert(url.Values{"": {"2"}})
 
 Non-capturing groups are ignored, but named capturing groups can be filled
-using a map (or a slice to treat them as positional variables):
+normally. Just set the key in url.Values:
 
 	regexp, err := reverse.Compile(`/foo/1(?P<two>\d+)3`)
 	if err != nil {
 		panic(err)
 	}
 	// url is "/foo/123".
-	url, err := re.Revert(nil, map[string]string{"two": "2"})
+	url, err := re.Revert(url.Values{"two": {"2"}})
 
 There are a few limitations that can't be changed:
 
 1. Nested capturing groups are ignored; only the outermost groups become
 a placeholder. So in `1(\d+([a-z]+))3` there is only one placeholder
-although there are two capturing groups: re.Revert([]string{"2", "a"}, nil)
+although there are two capturing groups: re.Revert(url.Values{"": {"2", "a"}})
 results in "123" and not "12a3".
 
 2. Literals inside capturing groups are ignored; the whole group becomes
